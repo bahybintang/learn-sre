f20ac9b856fcc86a6e8776eb958363ae44fbb5aa
Muhammad Bintang Bahy <bahybintang@gmail.com>
2021-11-13 07:54:26 +0700

    add dns google

---
+++ terraform/gcp/dns.tf
@@ -0,0 +1,22 @@
+resource "google_dns_record_set" "rootshell-backend" {
+  name         = "backend.${google_dns_managed_zone.rootshell.dns_name}"
+  managed_zone = google_dns_managed_zone.rootshell.name
+  type         = "A"
+  ttl          = 300
+
+  rrdatas = [resource.google_container_cluster.skripsi_cluster.endpoint]
+}
+
+resource "google_dns_record_set" "rootshell-frontend" {
+  name         = "frontend.${google_dns_managed_zone.rootshell.dns_name}"
+  managed_zone = google_dns_managed_zone.rootshell.name
+  type         = "A"
+  ttl          = 300
+
+  rrdatas = [resource.google_container_cluster.skripsi_cluster.endpoint]
+}
+
+resource "google_dns_managed_zone" "rootshell" {
+  name     = "rootshell"
+  dns_name = "rootshell.my.id."
+}
--- terraform/gcp/output.tf
+++ terraform/gcp/output.tf
@@ -1,3 +1,7 @@
 output "BUILD_IP" {
   value = resource.google_compute_instance.build_server.network_interface.0.access_config.0.nat_ip
 }
+
+output "CLUSTER_IP" {
+  value = resource.google_container_cluster.skripsi_cluster.endpoint
+}
