208299a18761f6a3d5d33cf9775284193f6b96f9
Muhammad Bintang Bahy <bahybintang@gmail.com>
2021-11-13 07:55:56 +0700

    update wildcard backend dns

--- terraform/gcp/dns.tf
+++ terraform/gcp/dns.tf
@@ -1,5 +1,5 @@
 resource "google_dns_record_set" "rootshell-backend" {
-  name         = "backend.${google_dns_managed_zone.rootshell.dns_name}"
+  name         = "*.backend.${google_dns_managed_zone.rootshell.dns_name}"
   managed_zone = google_dns_managed_zone.rootshell.name
   type         = "A"
   ttl          = 300
